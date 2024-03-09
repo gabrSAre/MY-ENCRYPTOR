ff178790180ca6fda4fccaf4585687292c4ed34f
Gabriel Salgado <gabrsare4593@gmail.com>
2024-03-09 18:59:51 -0500

    Ensallos con java script, solo prueba de repaso, y actualización de diseño responsivo en el archivo css

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-alert('DIGITA UN NÚMERO ENTRE 1 A 10');
-
-
-let numeroSecreto=6;
+let numeroSecreto= Math.floor(Math.random()*1000)+1;
+console.log(numeroSecreto);
 let numeroUsuario=0;
+let intentos=1;
 
 
 
 while(numeroUsuario!=numeroSecreto){
+    
 
-    numeroUsuario = prompt('ingresa un número entre 1 y 10'); 
-    console.log(numeroUsuario);
+    numeroUsuario = parseInt(prompt('ingresa un número entre 1 y 10')); 
+    console.log(typeof(numeroUsuario));
 
     
 
     if(numeroUsuario==numeroSecreto){
 
-        alert(`Adivinaste, el numero secreto es: ${numeroUsuario}`);
-
-
+        alert(`Adivinaste en ${intentos} ${intentos==1? 'vez':'veces'} , el numero secreto es: ${numeroUsuario}`);
     }
 
     else if(numeroUsuario>numeroSecreto){
@@ -28,10 +26,21 @@ while(numeroUsuario!=numeroSecreto){
         alert('El número secreto es mayor');
     }
 
-  
+    
+    if(intentos==10){
+        alert('Perdiste, el número de intentos se agotó');
+        break;
+    }
+
+   intentos++;
 
  
 
 }
+ 
+   
+     
+   
+    
 
 //DTMLJC+
